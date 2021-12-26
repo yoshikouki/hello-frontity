94eb11f8e0cad0083d31bf3170f96363378a1404
yoshikouki <yoshikouki@gmail.com>
2021-12-26 23:45:08 +0900

    Frontity state に接続する

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,9 +1,12 @@
-const Root = () => {
+import { connect } from "frontity";
+
+const Root = connect(({ state }) => {
   return (
     <>
       <h1>Hello, Frontity from components!!1</h1>
+      <p>Current URL: {state.router.link}</p>
     </>
   );
-};
+});
 
 export default Root
