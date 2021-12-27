820d66c71aed17ca62585779078af5d913b46813
yoshikouki <yoshikouki@gmail.com>
2021-12-27 22:51:21 +0900

    2️⃣ Adding a menu

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,10 +1,19 @@
+import Link from "@frontity/components/link";
 import { connect } from "frontity";
+import React from "react";
 
 const Root = connect(({ state }) => {
   return (
     <>
       <h1>Hello, Frontity from components!!1</h1>
       <p>Current URL: {state.router.link}</p>
+      <nav>
+        <ul>
+          <li><Link link="/">Home</Link></li>
+          <li><Link link="/page/2">More posts</Link></li>
+          <li><Link link="/about-us">About Us</Link></li>
+        </ul>
+      </nav>
     </>
   );
 });
