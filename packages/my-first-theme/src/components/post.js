13fcea9783735bf059f2e87a4728fc871b10d815
yoshikouki <yoshikouki@gmail.com>
2021-12-30 12:22:32 +0900

    投稿日を YYY-MM-DD に整形

--- packages/my-first-theme/src/components/post.js
+++ packages/my-first-theme/src/components/post.js
@@ -1,9 +1,11 @@
 import { connect } from "frontity"
 import React from "react"
+import dayjs from "dayjs";
 
 const Post = connect(({ state }) => {
   const data = state.source.get(state.router.link)
   const post = state.source[data.type][data.id]
+  const formattedDate = dayjs(post.date).format("YYYY-MM-DD")
   const author = state.source.author[post.author]
 
   return (
@@ -11,7 +13,7 @@ const Post = connect(({ state }) => {
       <h2>{post.title.rendered}</h2>
       <div>
         <strong>Posted: </strong>
-        {post.date}
+        {formattedDate}
       </div>
       <div>
         <strong>Author: </strong>
